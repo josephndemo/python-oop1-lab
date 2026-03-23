ccb96efb8441f67ba6b807843009a58acbffed45
Joseph Ndemo <joseph.ndemo@student.moringaschool.com>
2026-03-23 19:06:17 -0400

    1st commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,21 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count  # uses setter
+
+    # Getter
+    @property
+    def page_count(self):
+        return self._page_count
+
+    # Setter with validation
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    # Method
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
